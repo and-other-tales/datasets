212922fbeff631d203de3ee550d5939b7df63df2
fortunestoldco <david@fortunestold.co>
2025-05-02 09:10:56 +0100

    v0.1

--- src/othertales/datasets/__init__.py
+++ src/othertales/datasets/__init__.py
@@ -1,5 +1,11 @@
 """Dataset Creator Agent Package"""
 
+# This is a namespace package
+try:
+    __import__('pkg_resources').declare_namespace(__name__)
+except ImportError:
+    __path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
 from .dataset_agent import app, build_agent, build_graph
 from .llm_utils import get_llm
 
